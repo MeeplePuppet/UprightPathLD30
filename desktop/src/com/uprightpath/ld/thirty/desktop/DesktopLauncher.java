4a0f8e5e79a366c3a5c1b156d895b71bab997f2f
Geoffrey Owen <geoffrey.i.owen@gmail.com>
2014-08-22 17:53:36 -0600

    Initial Upload
    Initial Upload including the idea setup for the project.

---
+++ desktop/src/com/uprightpath/ld/thirty/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.uprightpath.ld.thirty.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.uprightpath.ld.thirty.UprightPathLD30;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UprightPathLD30(), config);
+	}
+}
---
+++ html/src/com/uprightpath/ld/thirty/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.uprightpath.ld.thirty.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.uprightpath.ld.thirty.UprightPathLD30;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new UprightPathLD30();
+        }
+}
